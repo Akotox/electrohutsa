392d2cebb411ce1326cbd7f894943cba357ba3e9
Carnage <akotoxmpimbo@gmail.com>
2025-04-24 00:19:14 +0200

    Add team member cards and integrate react-icons dependency

--- src/scenes/contact/index.tsx
+++ src/scenes/contact/index.tsx
@@ -1,11 +1,85 @@
-import { SelectedPage, teamMembers } from "../../shared/types";
+import { SelectedPage,  } from "../../shared/types";
 import { motion } from "framer-motion";
-import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.webp";
-import HText from "../../shared/HText";
+import TeamMemberCard from "./TeamCard";
+// import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.webp";
+// import HText from "../../shared/HText";
+
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
+const teamMembers = [
+    {
+      image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745446338/zco2gyhbwchkpdfdqmuf.jpg",
+      name: "Listen Mpimbo",
+      role: "CEO/Co-founder",
+      phone: "+27634443944",
+      socialLinks: [
+        
+      ],
+    },
+
+    {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745445920/lqt27vzxmhyjdifrpqak.jpg",
+        name: "Zaida Wagner",
+        role: "CEO/Co-founder",
+        phone: "+27794068845",
+        socialLinks: [
+          
+        ],
+      },
+
+      {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745445920/fzzdcx4zqg4uvfm1qv7q.jpg",
+        name: "Abrahamson",
+        role: "Tech Department",
+        phone: "+27815153706",
+        socialLinks: [
+          
+        ],
+      },
+      {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745445920/mhthdqsu4ayrmsno3zmf.jpg",
+        name: "Justice",
+        role: "Operations manager",
+        phone: "+27834377216",
+        socialLinks: [
+          
+        ],
+      },
+
+      {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745446684/ygvx9nbswgybqxfy3lpv.jpg",
+        name: "Andre",
+        role: "Tech Department",
+        phone: "+27631192050",
+        socialLinks: [
+          
+        ],
+      },
+
+      {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745445919/oqvapzothauqfmmer4cs.jpg",
+        name: "Micheil",
+        role: "Sales Department",
+        phone: "+27769050716",
+        socialLinks: [
+          
+        ],
+      },
+
+
+      {
+        image: "https://res.cloudinary.com/derwdq9ut/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1745445920/ahnj36pdktuyonkcqhef.jpg",
+        name: "Khanyiso Xhaya",
+        role: "Sales Department",
+        phone: "+27780077851",
+        socialLinks: [
+          
+        ],
+      },
+    // Add more team members here
+  ];
 
 const ContactUs = ({ setSelectedPage }: Props) => {
 
@@ -14,91 +88,19 @@ const ContactUs = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
       >
-        <motion.div
-          className="md:w-3/5"
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-          <HText>
-            <span className="text-primary-500">MEET OUR</span> TEAM
-          </HText>
-          <p className="my-5">
-            Get in touch with our dedicated team members for any inquiries or support. We are here to assist you with your Apple device needs!
-          </p>
-        </motion.div>
-
-      
-
-        {/* FORM AND IMAGE */}
-        <div className="mt-10 justify-between gap-8 md:flex">
-          <motion.div
-            className="mt-10 basis-3/5 md:mt-0"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.5 }}
-            variants={{
-              hidden: { opacity: 0, y: 50 },
-              visible: { opacity: 1, y: 0 },
-            }}
-          >
-            <div className="mt-10 flex flex-wrap justify-between gap-8">
-          {teamMembers.map((member) => (
-            <motion.div
-              key={member.id}
-              className="flex flex-col items-center basis-1/3 md:basis-1/4"
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ duration: 0.5 }}
-              variants={{
-                hidden: { opacity: 0, y: 50 },
-                visible: { opacity: 1, y: 0 },
-              }}
-            >
-              <div className="w-40 h-40 rounded-full overflow-hidden shadow-lg">
-                <img
-                  src={member.image}
-                  alt={member.name}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <h3 className="mt-4 text-lg font-semibold">{member.name}</h3>
-              <a href={`https://wa.me/${member.phone}`}>
-              <p className="text-primary-500">{member.phone}</p>
-
-              </a>
-            </motion.div>
-          ))}
-        </div>
-          </motion.div>
-
-          <motion.div
-            className="relative mt-16 basis-2/5 md:mt-0"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ delay: 0.2, duration: 0.5 }}
-            variants={{
-              hidden: { opacity: 0, y: 50 },
-              visible: { opacity: 1, y: 0 },
-            }}
-          >
-            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
-              <img
-                className="w-full"
-                alt="contact-us-page-graphic"
-                src={ContactUsPageGraphic}
-              />
-            </div>
-          </motion.div>
-        </div>
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">
+      <div className="mx-auto mb-8 max-w-screen-sm lg:mb-16">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our team</h2>
+        <p className="font-light text-black sm:text-xl dark:text-gray-400">
+  Meet the innovative minds behind ElectroHut — a dedicated team driving smart tech solutions and top-tier customer experiences.
+</p>
+      </div>
+      <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+        {teamMembers.map((member, index) => (
+          <TeamMemberCard key={index} name={member.name} role={member.role} imageUrl={member.image} phone={member.phone}  />
+        ))}
+      </div>
+    </div>
       </motion.div>
     </section>
   );
